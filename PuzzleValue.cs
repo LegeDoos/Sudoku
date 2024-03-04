53ab307eae7472bc56da2a2c7475263ba2201ebd
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2024-03-04 13:51:17 +0100

    Comments

--- PuzzleValue.cs
+++ PuzzleValue.cs
@@ -6,16 +6,29 @@ using System.Threading.Tasks;
 
 namespace Sudoku
 {
+    /// <summary>
+    /// Value of an element in the puzzle
+    /// </summary>
     public class PuzzleValue
     {
+        /// <summary>
+        /// Possible values the element can have
+        /// </summary>
         bool[] possibleValues;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public PuzzleValue()
         {
             // pos 0 = 1, etc
             possibleValues = [true, true, true, true, true, true, true, true, true];
         }
 
+        /// <summary>
+        /// Format a string with the possible values for the element
+        /// </summary>
+        /// <returns></returns>
         public string ShowPossibleValues()
         {
             string retVal = string.Empty;
@@ -27,6 +40,11 @@ namespace Sudoku
             return retVal;
         }
 
+        /// <summary>
+        /// Helper function to translate the boolean to a number
+        /// </summary>
+        /// <param name="pos">The position in the array</param>
+        /// <returns>The readable value of the element</returns>
         private string PosToVal(int pos)
         {
             return possibleValues[pos] ? $"{pos + 1}" : " ";
