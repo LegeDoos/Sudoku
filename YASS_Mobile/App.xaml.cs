a4d373a7efa81f5503e6e3f543fccc2dc4593825
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2024-11-19 17:00:18 +0100

    Add YASS_Mobile project with multi-platform support
    Added a new project named "YASS_Mobile" to the Visual Studio solution file (YASS.sln). This project supports multiple platforms, including Android, iOS, MacCatalyst, Windows, and Tizen, using .NET MAUI. Key additions include:
    
    - `App.xaml` and `App.xaml.cs` for the main application class.
    - `AppShell.xaml` and `AppShell.xaml.cs` for the shell structure.
    - `MainPage.xaml` and `MainPage.xaml.cs` for the main page with a button that counts clicks.
    - `MauiProgram.cs` for configuring the .NET MAUI application.
    - `YASS_Mobile.csproj` for project settings and dependencies.
    - Platform-specific files for Android, iOS, Tizen, and Windows.
    - `appicon.svg` for the application icon.
    - `AboutAssets.txt` for instructions on deploying raw assets.
    - `splash.svg` for the splash screen image.
    - `Colors.xaml` and `Styles.xaml` for defining color resources and UI styles.

---
+++ YASS_Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace YASS_Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ YASS_Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace YASS_Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
