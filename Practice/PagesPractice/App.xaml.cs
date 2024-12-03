0ca9eda2357b4d70dbf2dd9b949e665d90ea4c98
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2024-12-03 12:19:15 +0100

    New sulution Update Nuget

---
+++ Practice/PagesPractice/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace PagesPractice
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Practice/PagesPractice/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PagesPractice
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
